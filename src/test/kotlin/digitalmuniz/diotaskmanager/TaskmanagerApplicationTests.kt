f18d7d41aa2be3a1cf39082db49bff9488bf314f
Joseesanto14 <joseelias.12muniz@gmail.com>
2026-07-30 10:35:55 -0300

    initial commit/baseline

---
+++ src/test/kotlin/digitalmuniz/diotaskmanager/TaskmanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package digitalmuniz.diotaskmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
